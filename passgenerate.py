3449f5109db9e6fab55e8e63995a133b393b162e
aditya041003 <131935649+aditya041003@users.noreply.github.com>
2023-04-29 17:02:25 +0530

    Update passgenerate.py

--- passgenerate.py
+++ passgenerate.py
@@ -3,4 +3,4 @@ def generate():
     n = random.randint(8,12)
     l = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*'
     return ''.join(random.choices(l,k=n))
-print("The generated OTP is ",generate())
+print("The generated password is ",generate())
